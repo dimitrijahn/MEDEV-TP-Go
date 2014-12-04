321ec0e91c85bdb306d408c1418acfe5688ea4ac
dimitrijahn <dimitri.jahn@gmail.com>
2014-12-04 08:20:06 +0100

    Initialisation projet

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 4 décembre 2014, 08:18
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world" << endl;
+    
+    return 0;
+}
+
