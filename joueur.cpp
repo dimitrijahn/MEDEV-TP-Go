77ebb74308dd7e1ecd479c006a282de4a98a2d35
dimitrijahn <dimitri.jahn@gmail.com>
2014-12-04 09:56:25 +0100

    Fonction jouable

--- joueur.cpp
+++ joueur.cpp
@@ -1,13 +1,15 @@
 #include "joueur.h"
+#include "plateau.h"
 
+using namespace std;
 
 bool Joueur::libre(int x, int y)                // true si la case est libre
 {
     for (int i = p->pierres.size()-1; i >= 0; i--)      
     {
-        if (p->pierres[i].getX == x && p->pierres[i].getY == y)    // On cherche la derniere pierre à avoir ces coordonees
+        if (p->pierres[i]->getX() == x && p->pierres[i].getY() == y)    // On cherche la derniere pierre à avoir ces coordonees
         {
-            if (p->pierres[i].estVivant())                         // On regarde si elle est morte : true
+            if (p->pierres[i]->estVivant())                         // On regarde si elle est morte : true
             {
                 return false;
             }
@@ -22,9 +24,19 @@ bool Joueur::libre(int x, int y)                // true si la case est libre
 
 bool Joueur::jouable(int x, int y)
 {
-    if ( !repetition(int x, int y) && libre(int x, int y))
+    if ( !repetition(x, y) && libre(x, y))
     {
         return true;
     }
     return false;
+}
+
+
+bool Joueur::jouer(int x, int y)
+{
+    if (jouable(x, y))
+    {
+        Pierre *a = new Pierre (x, y, this->lettre, true);
+        p->pierres.push_back(a);
+    }
 }
--- plateau.cpp
+++ plateau.cpp
@@ -1,5 +1,17 @@
+
+#include "plateau.h"
+
 include "plateau.h";
 
 Plateau::Plateau() {
     
+}
+
+
+Pierre::Pierre(int x, int y, char lettre, bool vivant)
+{
+    this->x = x;
+    this->y = y;
+    this->lettre = lettre;
+    this->vivant = vivant;
 }
